df1381f597673e69dcb7a7da0491291362b9481f
alanvianaa <alanviana.nascimento@gmail.com>
2020-01-25 22:51:08 -0300

    gitignore

--- .history/src/resolvers_20191227151723.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    Query: {
-      planos: (_, __, { dataSources }) =>
-        dataSources.contaAPI.getPlanos(),
-      situacaoClientes: (_, __, { dataSources }) =>
-      dataSources.contaAPI.getSituacaoClientes()
-    }
-  };
